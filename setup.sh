d19fced5b1505443c3ca9799d94dc3ea1a3681f9
Sayed Ali <devsayed1@gmail.com>
2017-09-27 14:56:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,6 @@ echo " #    │Installing Sublime Text│"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt install sublime-text
-echo " #    │Installing Apache server│"
-wget https://raw.githubusercontent.com/ahmedash95/php-installer/master/ubuntu.sh && sudo sh ubuntu.sh
 echo " #    │Installing Chromium Browser│"
 sudo apt install chromium-browser
 echo " #    │Generating SSH Key│"
